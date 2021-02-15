8c3f5af61037c5a302aea38ba7f0af9548c2b659
Colton Swingle <coltonswingle@gmail.com>
2021-02-14 16:44:44 -0800

    adding data + changing script

--- video_to_images.py
+++ video_to_images.py
@@ -11,9 +11,9 @@ def main():
     )
     parser.add_argument('video_file', type=str,
                         help='Path to file of video')
-    parser.add_argument('s', '--save_dir', type=str, 
+    parser.add_argument('-s', '--save_dir', type=str,
                         help='Parent directory for saving')
-    parser.add_argument('-r', '--rate', type=float, 
+    parser.add_argument('-r', '--rate', type=float,
                         help='Frame rate')
     args = parser.parse_args()
 
@@ -24,17 +24,20 @@ def main():
     out_dir = osp.splitext(osp.basename(video_file))[0]
     if save_dir:
         out_dir = osp.join(save_dir, out_dir)
-    os.makedirs(out_dir)
 
+    if not osp.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # TODO: crop video to desired length in code
     reader = imageio.get_reader(video_file)
     meta_data = reader.get_meta_data()
     fps = meta_data['fps']
-    print(fps)
     n_frames = meta_data['nframes']
 
+    print(f'Processing video for {n_frames}')
     for i, img in tqdm.tqdm(enumerate(reader), total=n_frames):
         if rate is None or i % int(round(fps / rate)) == 0:
-            imageio.imsave(osp.join(out_dir, '%08d.png' % i), img)
+            imageio.imsave(osp.join(out_dir, str(i) + '.png'), img)
 
 
 if __name__ == '__main__':
