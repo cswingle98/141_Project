01dcc54fbcaa978ff4f61fbae6320722e8d77b2d
Colton Swingle <coltonswingle@gmail.com>
2021-02-16 16:08:24 -0800

    Script for converting annotated images to video format

---
+++ images_to_video.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import tqdm
+import cv2
+
+
+def main(pic_dir, save_dir):
+    video_name = 'annotated.avi'
+    if save_dir:
+        video_path = os.path.join(save_dir, video_name)
+    else:
+        video_path = video_name
+
+    images = [img for img in os.listdir(pic_dir) if img.endswith(".png") or img.endswith('.jpg')]
+
+    # sort images to appear in list in order they were taken
+    images = sorted(images, key=lambda x: int(x.split('.')[0]))
+    frame = cv2.imread(os.path.join(pic_dir, images[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(video_path, 0, 5, (width, height))
+
+    for img in tqdm.tqdm(images):
+        video.write(cv2.imread(os.path.join(pic_dir, img)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', type=str,
+                        help='Directory holding images')
+    parser.add_argument('-r', '--rate', type=int, default=5,
+                        help='Frame rate. Defaults to 5')
+    parser.add_argument('-s', '--save', default=None,
+                        help='Save path. Defaults to current path')
+    args = parser.parse_args()
+    pic_dir, save_dir = args.dir, args.save
+
+    main(pic_dir, save_dir)
